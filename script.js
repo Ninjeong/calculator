b80a5e35028fc5e93b6a3bc91169a77ad4f4ebef
Ninjeong <nathan508snap@gmail.com>
2026-07-01 13:23:55 -0400

    Complete main functionality and logic in script.js

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-let leftOperand = 0;
-let rightOperand = 0;
+let leftOperand = "";
+let rightOperand = "";
 let operator = "";
+let operatorClicked = false;
+
+const clearBtn = document.querySelector("#clearBtn");
+
+const sevenBtn = document.querySelector("#Btn7");
+const eightBtn = document.querySelector("#Btn8");
+const nineBtn = document.querySelector("#Btn9");
+const divideBtn = document.querySelector("#divideBtn");
+
+const fourBtn = document.querySelector("#Btn4");
+const fiveBtn = document.querySelector("#Btn5");
+const sixBtn = document.querySelector("#Btn6");
+const multiplyBtn = document.querySelector("#multiplyBtn");
+
+const oneBtn = document.querySelector("#Btn1");
+const twoBtn = document.querySelector("#Btn2");
+const threeBtn = document.querySelector("#Btn3");
+const subBtn = document.querySelector("#subBtn");
+
+const zeroBtn = document.querySelector("#Btn0");
+const decimalBtn = document.querySelector("#decimalBtn");
+const equalBtn = document.querySelector("#equalBtn");
+const addBtn = document.querySelector("#addBtn");
+
+const mainDisplay = document.querySelector(".mainDisplay");
 
 function add(leftOperand, rightOperand) {
     return leftOperand + rightOperand;
@@ -30,10 +55,90 @@ function operate(leftOperand, operator, rightOperand) {
     }
     else if (operator === "÷") {
         if (rightOperand === 0) {
-            alert("Error! Cannot divide by 0!")
+            alert("Error! Cannot divide by 0!");
+            clear();
         }
         else {
             return divide(leftOperand, rightOperand);
         }
     }
 }
+
+function appendDigit(digit) {
+    if (operatorClicked === false) {
+        leftOperand += digit;
+    }
+    else {
+        rightOperand += digit;
+    }
+    mainDisplay.textContent = operatorClicked ? rightOperand : leftOperand;
+
+}
+
+function clear() {
+    leftOperand = "";
+    rightOperand = "";
+    operatorClicked = false;
+    operator = "";
+    mainDisplay.textContent = "";
+}
+
+function setOperator(operatorSymbol) {
+    if (leftOperand !== "" && rightOperand !== "") {
+        // both numbers exist, calculate first
+        leftOperand = String(operate(Number(leftOperand), operator, Number(rightOperand)));
+        rightOperand = "";
+        mainDisplay.textContent = leftOperand;
+    }
+    operator = operatorSymbol;
+    operatorClicked = true;
+}
+
+clearBtn.addEventListener("click", () => clear());
+
+oneBtn.addEventListener("click", () =>  {
+    if (operatorClicked === false && mainDisplay.textContent !== "") {
+        clear();
+    }
+    appendDigit("1");
+});
+twoBtn.addEventListener("click", () => {
+    if (operatorClicked === false && mainDisplay.textContent !== "") {
+        clear();
+    }
+    appendDigit("2");
+});
+threeBtn.addEventListener("click", () => {
+    if (operatorClicked === false && mainDisplay.textContent !== "") {
+        clear();
+    }
+    appendDigit("3");
+});
+fourBtn.addEventListener("click", () => appendDigit("4"));
+fiveBtn.addEventListener("click", () => appendDigit("5"));
+sixBtn.addEventListener("click", () => appendDigit("6"));
+sevenBtn.addEventListener("click", () => appendDigit("7"));
+eightBtn.addEventListener("click", () => appendDigit("8"));
+nineBtn.addEventListener("click", () => appendDigit("9"));
+zeroBtn.addEventListener("click", () => appendDigit("0"));
+decimalBtn.addEventListener("click", () => appendDigit("."));
+
+addBtn.addEventListener("click", () => setOperator("+"));
+subBtn.addEventListener("click", () => setOperator("-"));
+multiplyBtn.addEventListener("click", () => setOperator("x"));
+divideBtn.addEventListener("click", () => setOperator("÷"));
+
+equalBtn.addEventListener("click", () => {
+    if (leftOperand !== "" && rightOperand !== "" && operator !== "") {
+        const result = parseFloat(operate(Number(leftOperand), operator, Number(rightOperand)).toFixed(10));
+        mainDisplay.textContent = result;
+        leftOperand = String(parseFloat(result.toFixed(10)));
+        rightOperand = "";
+        operator = "";
+        operatorClicked = false;
+    }
+    else {
+        alert("Invalid input, please input operands");
+        clear();
+    }
+});
